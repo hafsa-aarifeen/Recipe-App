227261ca23cb5a42e56a3541972af962fa92311b
hafsa-aarifeen <hafsama-im19042@stu.kln.ac.lk>
2023-08-09 19:22:18 +0530

    update

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { styled } from "styled-components";
 import { motion } from "framer-motion";
 import { Link, useParams } from "react-router-dom";
 
 function Cuisine() {
+
+    const [cuisine, setCuisine] = useState ([]);
+    let params = useParams();
+
+    const getCuisine =  async (name) => {
+        const data = await fetch (`GET https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`
+        );
+        const receipes = await data.json();
+        setCuisine(receipes.results);
+    }
+
+    useEffect(() => {
+        // getCuisine('italian')
+        console.log(params);
+    },[]);
+
     return <div>
         
     </div>;
